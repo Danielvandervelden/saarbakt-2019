ab37ed2388e2f6069f2d3c7f12eb74259a7868e6
danielvandervelden <daniel@42functions.nl>
2019-06-30 12:01:52 +0200

    Removed twentynineteen theme.

--- wp-content/themes/saarbakt-2019/functions.php
+++ wp-content/themes/saarbakt-2019/functions.php
@@ -2,7 +2,7 @@
 
 function saarbakt_theme_files() {
 	wp_enqueue_style('saarbakt_styles', get_stylesheet_directory_uri() . '/assets/styles/main.css', false);
-	wp_register_script('saarbakt_scripts', get_stylesheet_directory_uri() . '/assets/scripts/main.js', array(), '1.0.0', true);
+	wp_register_script('saarbakt_scripts', get_stylesheet_directory_uri() . '/assets/scripts/main.js', array (), 1.1, true);
 	wp_enqueue_script('saarbakt_scripts');
 }
 
