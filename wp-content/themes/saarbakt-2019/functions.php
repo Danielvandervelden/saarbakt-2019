24b117e0b7eb56c50b1258a77c4a5928a4e2098b
danielvandervelden <daniel@42functions.nl>
2019-06-30 12:20:03 +0200

    Sometimes I realllly don't like wordpress.

--- wp-content/themes/saarbakt-2019/functions.php
+++ wp-content/themes/saarbakt-2019/functions.php
@@ -2,8 +2,8 @@
 
 function saarbakt_theme_files() {
 	wp_enqueue_style('saarbakt_styles', get_stylesheet_directory_uri() . '/assets/styles/main.css', false);
-	wp_register_script('saarbakt_scripts', get_stylesheet_directory_uri() . '/assets/scripts/main.js', array (), 1.1, true);
-	wp_enqueue_script('saarbakt_scripts');
+	wp_register_script( 'saarbakt_scripts', get_template_directory_uri() . '/assets/scripts/main.js', array(), NULL, true );
+    wp_enqueue_script( 'saarbakt_scripts' );
 }
 
 add_action('wp_enqueue_scripts', 'saarbakt_theme_files');
