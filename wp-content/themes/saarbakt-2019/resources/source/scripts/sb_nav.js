1297b675022a1cb9f860cabe27d4a5fc6f6ac9fc
danielvandervelden <daniel@42functions.nl>
2019-07-20 15:42:09 +0200

    Fixed desktop menu.

--- wp-content/themes/saarbakt-2019/resources/source/scripts/sb_nav.js
+++ wp-content/themes/saarbakt-2019/resources/source/scripts/sb_nav.js
@@ -1,11 +1,14 @@
 import _ from './helper/HelperFunctions';
-
 class SbNav {
 	constructor() {
-		this.navToggle = document.getElementById('sb-nav-toggle');
-		this.recipeToggle = document.querySelector('#sb-menu .menu-item-has-children');
-		this.backToggle = document.getElementById('sb-back-toggle');
 		this.body = document.body;
+
+		this.megaMenuToggle = document.querySelectorAll('#sb-menu .menu-item-has-children');
+		this.menu = document.getElementById('sb-menu');
+
+		this.navToggleMobile = document.getElementById('sb-nav-toggle');
+		this.recipeToggleMobile = document.querySelector('#sb-menu .menu-item-has-children');
+		this.backToggleMobile = document.getElementById('sb-back-toggle');
 
 		if(window.innerWidth <= 1024) {
 			this.initMobileNav();
@@ -15,23 +18,56 @@ class SbNav {
 	}
 
 	initMobileNav() {
-		this.navToggle.addEventListener(_.clickEvent(), function() {
+		this.navToggleMobile.addEventListener(_.clickEvent(), function() {
 			this.body.classList.toggle('menu-open');
 		}.bind(this))
 
-		this.recipeToggle.addEventListener(_.clickEvent(), function(e) {
+		this.recipeToggleMobile.addEventListener(_.clickEvent(), function(e) {
 			e.preventDefault();
 			this.body.classList.add('submenu-active');
 		}.bind(this));
 
-		this.backToggle.addEventListener(_.clickEvent(), function(e) {
+		this.backToggleMobile.addEventListener(_.clickEvent(), function(e) {
 			e.preventDefault();
 			this.body.classList.remove('submenu-active');
 		}.bind(this));
 	}
 
 	initDesktopNav() {
-		
+		this.megaMenuToggle.forEach(megaMenuItem => {
+			megaMenuItem.addEventListener(_.clickEvent(), function(e) {
+				if(e.currentTarget === megaMenuItem && e.target.closest('.menu-item').classList.contains('menu-item-has-children')) {
+					e.preventDefault();
+					if(!document.querySelector('.backdrop')) {
+						this.openMenu(megaMenuItem);
+						this.setBackdropEventListener(megaMenuItem);
+					} else {
+						this.closeMenu(megaMenuItem);
+					}
+				}
+			}.bind(this))
+		})
+	}
+
+	setBackdropEventListener(megaMenuItem) {
+		this.body.addEventListener(_.clickEvent(), function backdropListener(e) {
+			if(e.target === e.currentTarget) {
+				this.body.removeEventListener(_.clickEvent(), backdropListener);
+				this.closeMenu(megaMenuItem);
+			}
+		}.bind(this))
+	}
+
+	openMenu(megaMenuItem) {
+		this.body.classList.add('backdrop');
+		this.menu.classList.add('mega-menu-active');
+		megaMenuItem.classList.add('submenu-active');
+	}
+
+	closeMenu(megaMenuItem) {
+		this.body.classList.remove('backdrop');
+		this.menu.classList.remove('mega-menu-active');
+		megaMenuItem.classList.remove('submenu-active');
 	}
 }
 
