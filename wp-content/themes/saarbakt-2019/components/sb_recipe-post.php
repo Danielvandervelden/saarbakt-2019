c015551c1d2d42e3eb236c6ff4cc0da54a884b1d
danielvandervelden <daniel@42functions.nl>
2019-09-09 13:11:59 +0200

    Attempted fix for post thumbnail image size.

--- wp-content/themes/saarbakt-2019/components/sb_recipe-post.php
+++ wp-content/themes/saarbakt-2019/components/sb_recipe-post.php
@@ -1,6 +1,6 @@
 <article class="sb-recipe-post">
 	<h5><a href="<?= get_the_permalink() ?>"><?= get_the_title() ?></a></h5>
-	<a class="photo" href="<?= get_the_permalink() ?>"><img src="<?= get_the_post_thumbnail_url('image-mobile') ?>" alt="<?= get_the_title() ?>" /></a>
+	<a class="photo" href="<?= get_the_permalink() ?>"><img src="<?= get_the_post_thumbnail_url(null, 'image-mobile') ?>" alt="<?= get_the_title() ?>" /></a>
 	<div class="content"><?= wp_trim_words(get_the_content(), 70) ?></div>
 	<a href="<?= get_the_permalink() ?>" class="btn"><span>Lees meer</span></a>
 </article>
