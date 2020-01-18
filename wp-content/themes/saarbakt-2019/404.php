f9ea1e4049049781682da60c9553d37be61353b7
danielvandervelden <daniel@42functions.nl>
2020-01-18 21:59:04 +0100

    Fixed text for 404 page.

--- wp-content/themes/saarbakt-2019/404.php
+++ wp-content/themes/saarbakt-2019/404.php
@@ -3,7 +3,7 @@ get_header();
 ?>
 
 <section class="sb-container">
-	<h2 class="sb-page-title">Oh god nee toch...</h2>
+	<h2 class="sb-page-title">Oeps.. deze pagina kan ik niet vinden..</h2>
 	<img src="/wp-content/themes/saarbakt-2019/assets/images/saar404.png" alt="Saar snapt het ook allemaal niet meer..">
 	<p>Ik snap ook niet waar dat recept naartoe is gegaan.. Maar misschien is het niet verloren! Gebruik de zoekfunctionaliteit om het te vinden of <br></p>
 	<a class="btn" href="<?= get_home_url() ?>"><span>Ga terug naar de homepagina</span></a>
