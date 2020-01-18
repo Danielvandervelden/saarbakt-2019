d07dfc6e8edd0e08a28d0420427fcff0ede72d4a
danielvandervelden <daniel@42functions.nl>
2020-01-18 16:39:46 +0100

    Updated wordpress to 5.3

---
+++ wp-content/themes/saarbakt-2019/page.php
@@ -0,0 +1,15 @@
+<?php 
+get_header();
+?>
+
+<section class="sb-container sb-two-col list">
+	<div>
+		<h2 class="sb-page-title"><?= get_the_title() ?></h2>
+		<div><?= get_the_content(); ?></div>
+	</div>
+	<?= get_template_part('components/sb_instagram') ?>
+</section>
+
+<?php 
+get_footer();
+?>
