fa3e82affc4e3202350d6c9491291d9e37dc7cfd
danielvandervelden <daniel@42functions.nl>
2020-09-26 19:45:43 +0200

    Updated wordpress to 5.5.1

--- wp-content/themes/saarbakt/components/visual.php
+++ wp-content/themes/saarbakt/components/visual.php
@@ -1,57 +1,8 @@
 <header id="visual">
 	<figure id="logo">
-		<img data-src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
+		<img src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
 			alt="Saarbakt Logo" 
-			data-srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
+			srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
 			sizes="(min-width: 768px) 800px, 300px">
 	</figure>
-	<figure id="logo">
-		<img data-src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
-			alt="Saarbakt Logo" 
-			data-srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
-			sizes="(min-width: 768px) 800px, 300px">
-	</figure>
-	<figure id="logo">
-		<img data-src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
-			alt="Saarbakt Logo" 
-			data-srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
-			sizes="(min-width: 768px) 800px, 300px">
-	</figure>
-	<figure id="logo">
-		<img data-src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
-			alt="Saarbakt Logo" 
-			data-srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
-			sizes="(min-width: 768px) 800px, 300px">
-	</figure>
-	<figure id="logo">
-		<img data-src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
-			alt="Saarbakt Logo" 
-			data-srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
-			sizes="(min-width: 768px) 800px, 300px">
-	</figure>
-	<figure id="logo">
-		<img data-src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
-			alt="Saarbakt Logo" 
-			data-srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
-			sizes="(min-width: 768px) 800px, 300px">
-	</figure>
-	<figure id="logo">
-		<img data-src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
-			alt="Saarbakt Logo" 
-			data-srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
-			sizes="(min-width: 768px) 800px, 300px">
-	</figure>
-	<figure id="logo">
-		<img data-src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
-			alt="Saarbakt Logo" 
-			data-srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
-			sizes="(min-width: 768px) 800px, 300px">
-	</figure>
-	<figure id="logo">
-		<img data-src="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png"
-			alt="Saarbakt Logo" 
-			data-srcset="<?= get_template_directory_uri(); ?>/build/images/saarbaktlogo_mobile.png 320w, <?= get_template_directory_uri(); ?>/build/images/saarbaktlogo.png 720w"
-			sizes="(min-width: 768px) 800px, 300px">
-	</figure>
-	
 </header>
--- wp-content/themes/saarbakt/resource/source/scripts/lib/module.lazy-load.js
+++ wp-content/themes/saarbakt/resource/source/scripts/lib/module.lazy-load.js
@@ -3,25 +3,4 @@
  */
 import { toArray } from '../helper/module.helpers';
 
-const img = new Image();
 
-toArray(document.getElementsByTagName('img')).forEach(image => {
-	if("srcset" in img && IntersectionObserver) {
-		const observer = new IntersectionObserver(() => {
-			fixSource(image, 'srcset');
-		}, { root: null, rootMargin: '0px', threshold: 0.5 })
-
-		observer.observe(image);
-	} else {
-		fixSource(image, 'src');
-	}
-})
-
-function fixSource(image, dataAttribute) {
-	console.log('test');
-	image.setAttribute(dataAttribute, image.getAttribute(`data-${dataAttribute}`));
-	image.removeAttribute('data-src');
-	image.removeAttribute('data-srcset');
-
-	image.classList.add('lazy-loaded');
-}
